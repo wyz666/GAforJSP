ac2402e7685b3a655f67329a81229f39a0ad2ea4
wyz666 <954675837@qq.com>
2023-05-04 00:53:53 +0800

    提交GAforJSP文件夹所有文件

---
+++ job.py
@@ -0,0 +1,5 @@
+class Job():
+    def __init__(self, id, name, ops):
+        self.id = id
+        self.name = name
+        self.ops = ops
---
+++ task.py
@@ -0,0 +1,5 @@
+class Task():
+    def __init__(self, id, machine, duration):
+        self.id = id
+        self.machine = machine
+        self.duration = duration
